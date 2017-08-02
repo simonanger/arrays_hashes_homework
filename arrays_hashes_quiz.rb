5398ea66b385e57e43a79451dabc4cec94400f28
Simon Anger <simonanger89@gmail.com>
2017-08-02 16:50:10 +0100

    Answer questions until users hash

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -3,38 +3,43 @@
 lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'Princes Street']
 
 # 1. Work out how many stops there are in the array
-# puts lines.length()
+ puts lines.length()
 
 # 2. Return 'Edinburgh Park' from the array
+ puts lines[1]
 
 # 3. How many ways can we return 'Princes Street' from the array?
+  puts lines[4]
+  puts lines.last
+  puts lines[-1]
 
 # 4. Work out the index position of 'Haymarket'
+  puts lines[3]
 
 # 5. Add 'Airport' to the start of the array
-
+  lines.unshift['Airport']
 # 6. Add 'York Place' to the end of the array
-
+  lines.push['York Place']
 # 7. Remove 'Edinburgh Park' from the array using it's name
-
+  lines.delete 'Edinburgh Park'
 # 8. Delete 'Edinburgh Park' from the array by index
-
+  lines.delete_at(1)
 # 9. Reverse the positions of the stops in the array
-
+  lines.reverse()
 ### B. Given the following data structure:
 
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
-
+  return my_hash[1]
 # 2. How would you return the string `"Two"`?
-
+  return my_hash[:two]
 # 3. How would you return the number `2`?
-
+  return my_hash["two"]
 # 4. How would you add `{3 => "Three"}` to the hash?
-
+  my_hash[3] = "Three"
 # 5. How would you add `{:four => 4}` to the hash?
-
+  my_hash[:four] = 4
 ### C. Given the following data structure:
 
 users = {
---
+++ testing.rb
@@ -0,0 +1,8 @@
+my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
+
+
+
+
+my_hash[3] = "Three"
+my_hash[:four] = 4
+puts my_hash
